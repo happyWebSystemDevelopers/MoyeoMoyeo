1220e446cfd991ac54c6e677af0b5828de5d813b
nahyun <nahyun858@gmail.com>
2019-11-25 19:49:43 +0900

    add axios and store.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
 import 'es6-promise/auto'
+import axios from 'axios'
 
 Vue.use(Vuex)
 Vue.config.productionTip = false
-
+Vue.prototype.$Axios = axios;
 // eslint-disable-next-line no-unused-vars
 const store = new Vuex.Store({
   state:{
