023f322756e6f5eb8c6b2f361f948fd742cd6484
nahyun <nahyun858@gmail.com>
2019-12-05 11:04:03 +0900

    writeBoard 구현

--- frontend/src/view/WriteFreeBoard.vue
+++ frontend/src/view/WriteFreeBoard.vue
@@ -13,7 +13,12 @@
     <span style="font-size: 30px;">Image</span>
     <br>
     <br>
-    <input type="text" v-model="imageURL" @ style="height:25px; width: 1100px; border-left: none; border-right: none; border-top:none;" placeholder ="Image URL(optional)">
+    <input type="file" @change="fileUpload"> <!--첨부파일 누르는 부분-->
+    <!-- style="height:25px; width: 1100px; border-left: none; border-right: none; border-top:none;" placeholder ="Image URL(optional)">
+    <button id ="uploadButton" v-on:change="fileUpload">File Upload</button> 업로드 버튼-->
+    <br>
+    <br>
+    <div id = "userfileImg"/> <!--유저가 올린 이미지 사진-->
     <br>
     <br>
     <span style="font-size: 30px;" >Content</span>
@@ -29,21 +34,48 @@
 <script>
 import axios from 'axios';
 
+
 export default {
     name: 'writefreeboard',
     data() {
         return {
             title : '',
-            imageURL: '',
             content: '',
         }
     },
     methods: {
-        postContent: function() {
+        postContent: function() { //제목이랑 내용 없이 완료 버튼 누르면 경고창 나오는거
+            if(this.title == "" || this.content == "")
+            {
+                alert("Please write Title and Content")
+                this.title = '';
+                this.content ='';
+                document.getElementById("userfileImg").innerHTML='';
+                
+                //document.getElementById("userfileImg").removeChild(document.getElementById("userfileImg").firstChild);
+            }
+            else {
             axios.post("/writefreeboard",{title: this.title, url :this.imageURL, content : this.content});
             this.$router.push({
                 name: 'free'
             });
+            }
+        },
+        fileUpload : function(event) { //첨부파일을 올리는 메또드 
+            var fr = new FileReader();
+            var newImg = document.createElement("img");
+            fr.onload = function() {
+                 //inputImgSrc = fr.result;
+                 newImg.src = fr.result;
+                 this.imageURL = fr.result;
+                 newImg.classList.add("userUploadImg");
+                 newImg.style.height = '200px';
+                 newImg.style.width = '200px';
+                
+                 document.querySelector("#userfileImg").appendChild(newImg);
+            }
+            fr.readAsDataURL(event.target.files[0]);
+            //document.getElementById("userfileImg").innerHTML = 
         }
 
 
@@ -66,6 +98,8 @@ export default {
     background-color : white;
     border : 1.5px solid #9DC3C1;
 }
+
+
 
 
 </style>
