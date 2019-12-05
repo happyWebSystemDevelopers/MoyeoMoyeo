ea00ede711b7ad88a14f0b593fc7d33b015a11ff
sywtit <youwrothit17@naver.com>
2019-12-05 12:39:59 +0900

    push sign up process in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var CORS = require('cors');
+
+var app = express();
+app.use(CORS());
+
 const session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var mainRouter = require('./routes/mainBoard');
-
-var app = express();
+var universityRouter = require('./routes/university');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api/main', mainRouter);
-
+app.use('/api/universityList', universityRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
