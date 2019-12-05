723ddd06e9c46649b8f8039a48a120cded9551a3
nahyun <nahyun858@gmail.com>
2019-12-05 10:05:20 +0900

    freeboard modify

--- frontend/src/view/FreeBoard.vue
+++ frontend/src/view/FreeBoard.vue
@@ -1,11 +1,11 @@
 <template>
     <div class ="free">
-    <img id="freeboardImg" src="../assets/freeBoard.png" style ="height : 60px; width:220px; margin-left: 650px;">
+    <!--<img id="freeboardImg" src="../assets/freeBoard.png" style ="height : 60px; width:220px; margin-left: 650px;">
     <router-link to ="/writefreeboard"><button id="createButton" style ="margin-left: 400px; width:100px; height: 40px; font-size: 20px;">Create</button></router-link>
-    <hr align="left" style ="color : #dddfe6; border: 2px solid; margin-left: 120px; margin-right: 120px;"/>
+    <hr align="left" style ="color : #dddfe6; border: 2px solid; margin-left: 120px; margin-right: 120px;"/>-->
     <!--<div v-for = "board in freeBoards">-->
     <!-- 여기에다가 그 router-view를 두고 이미지에 흠...-->
-    <div class ="board" v-for ="(board, index) in freeBoards" :key ="board.title">
+    <!--div class ="board" v-for ="(board, index) in freeBoards" :key ="board.title">
     <router-link :to = "{name : 'freeBoardDetail', params: {idx : index}}"><img v-if="board.url" v-bind:src="'images/' + board.url" style="height : 200px; width: 260px;">
     <img v-else src ="images/poster_4.jpg" style="height : 200px; width: 260px;">
     <hr style ="boder-style : dotted; color : #E0E3DA; border : 1.2px solid;"/>
@@ -14,9 +14,9 @@
     <span style ="color : #566270;">{{ board.email }}</span>
     <hr style ="boder-style : dotted; color: #E0E3DA; border : 1.2px solid;"/>
     <span style ="color : #566270;">{{ board.title }}</span></router-link>
-    </div>
-    <img id="freeboardImg" src="../assets/freeBoard.png" style ="width:150px; margin-left: 600px;">
-    <button id="createButton" style ="margin-left: 310px;">Create</button>
+    </div>-->
+    <img id="freeboardImg" src="../assets/freeBoard.png" style ="height : 60px; width:220px; margin-left: 630px;">
+    <router-link to ="/writefreeboard"><button id="createButton" style ="margin-left: 350px; width:100px; height: 40px; font-size: 20px;">Create</button></router-link>
     <button v-if="viewMethod=='grid'" v-on:click="toLined" class="viewChange"><img class="btnImg" src="../assets/lined.png"></button> <!--그리드/라인 보기 방식 변경하는 버튼-->
     <button v-if="viewMethod=='lined'" v-on:click="toGrid" class="viewChange"><img class="btnImg" src="../assets/grided.png"></button>
     <hr align="left" style ="color : #dddfe6; border: 2px solid; margin-left: 120px; margin-right: 120px;"/>
@@ -25,13 +25,13 @@
         <div v-if="viewMethod=='grid'" id="gridBoard"> <!--원래 나현이가 만들었던 그리드 형식-->
             <div class ="board" v-for ="(board, index) in freeBoards" :key ="board.title">
                 <router-link :to = "{name : 'freeBoardDetail', params: {idx : index}}"><img v-if="board.url" v-bind:src="'images/' + board.url" style="height : 200px; width: 250px;">
-                    <img v-else src ="images/poster_4.jpg" style="height : 200px; width: 250px;"></router-link>
+                    <img v-else src ="images/poster_4.jpg" style="height : 200px; width: 250px;"> <!--default를 겨울왕국이미지로 했는데 이거 나중에 수정해야함-->
                 <hr style ="boder-style : dotted; color : #E0E3DA; border : 1.2px solid;"/>
                 <span style ="color : #566270;">{{ board.writer }}</span>
                 <br>
                 <span style ="color : #566270;">{{ board.email }}</span>
                 <hr style ="boder-style : dotted; color: #E0E3DA; border : 1.2px solid;"/>
-                <span style ="color : #566270;">{{ board.title }}</span>
+                <span style ="color : #566270;">{{ board.title }}</span></router-link>
             </div>
         </div>
 
@@ -88,7 +88,6 @@ var freeBoardExample = [{
 },
 {
     url : "freeBoardDefault.png", //그 첨부파일 하는거 과제 1 참고하기 
-    title : "Hi33",
     title : "VeeeeeeeeeeeeeeeeeeeeeeeeeeeryLooooooooooooooooooooooooongTiiiiiiiiiiiiiiiiiiiiiitle",
     email : "sooyoung1234@ajou.ac.kr",
     writer : "nahyun"
